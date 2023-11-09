f69da522b34fb8e875608f5a5b5084cca1761018
Monika Marek <62980345+monikamarr@users.noreply.github.com>
2023-11-09 09:58:57 -0800

    Implementing membership categorization for Western Suburbs Croquet Club. Criteria: Senior (age >= 55 and handicap > 7), Open otherwise. Input: List of age and handicap pairs. Output: List of corresponding categories (Senior/Open).

---
+++ membership.py
@@ -0,0 +1,8 @@
+def open_or_senior(data):
+    output = []
+    for i in range(len(data)):
+        if data[i][0] >= 55 and data[i][1] > 7:
+            output.append("Senior")
+        else:
+            output.append("Open")
+    return output
